598e6bf49f50a232722eb3d36e886afd83fcd0e3
Bernardo Fontes <falecomigo@bernardofontes.net>
2011-07-02 11:25:20 -0300

    Remove banco antigo

--- db/migrate/20100723211038_add_lat_long_index.rb
+++
@@ -1,9 +0,0 @@
-class AddLatLongIndex < ActiveRecord::Migration
-  def self.up
-    add_index :dojos, :lat_long, :unique => true
-  end
-
-  def self.down
-    remove_index :dojos, :lat_long
-  end
-end
