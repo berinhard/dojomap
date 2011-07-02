6e6de89f662fdc256be00fd71e97c5443ee5a3af
Bernardo Fontes <falecomigo@bernardofontes.net>
2011-07-02 11:29:53 -0300

    Adquea o js que faz a plotagem dos pontos no mapa

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 function initialize_map(position) {
-	var latlng; 
+	var latlng;
 	if(position){
 			latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
 	} else{
@@ -12,17 +12,17 @@ function initialize_map(position) {
   };
   var map = new google.maps.Map(document.getElementById("map_canvas"),
       myOptions);
-  
-  var image = '/images/dojo.png';
-  $.getJSON('/dojos', function(dojos) {
-    $(dojos).each(function(index, d) {
-			var dojo = d.dojo;
+
+  var image = '/images/coop.gif';
+  $.getJSON('/coops', function(coops) {
+    $(coops).each(function(index, d) {
+			var coop = d.coop;
       var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(dojo.latitude,dojo.longitude), 
-        map: map, 
+        position: new google.maps.LatLng(coop.latitude,coop.longitude),
+        map: map,
         icon: image
       });
-      $.get("/dojos/" + dojo.id, function(contentSting) {
+      $.get("/coops/" + coop.id, function(contentSting) {
 
         var infowindow = new google.maps.InfoWindow({
             content: contentSting
@@ -31,7 +31,6 @@ function initialize_map(position) {
         google.maps.event.addListener(marker, 'click', function() {
           infowindow.open(map,marker);
         });
-        
       });
     });
   });
@@ -42,7 +41,7 @@ $(document).ready(function(){
 });
 
 function initialize() {
-	if(navigator.geolocation) 
+	if(navigator.geolocation)
 			navigator.geolocation.getCurrentPosition(initialize_map, handle_errors);
 	else
 		initialize_map();
@@ -50,9 +49,9 @@ function initialize() {
 function handle_errors(error)
 {
 	switch(error.code) {
-		default: 
+		default:
 		initialize_map();
 		break;
-		
+
 	}
 }
